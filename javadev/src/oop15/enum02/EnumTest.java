2af0eef0521313b90526bb567b4e6a7812c9cedb
user <user@DESKTOP-C5EEQRQ>
2024-06-24 18:01:03 +0900

    EnumTest error fix

--- javadev/src/oop15/enum02/EnumTest.java
+++ javadev/src/oop15/enum02/EnumTest.java
@@ -5,13 +5,13 @@ import oop15.enum01.Person;
 
 public class EnumTest {
 	public static void main(String[] args) {
-		Person man = Person.WOMAN;
-		if (man == Person.MAN)
-			System.out.println("man");
-		else if (man == Person.WOMAN)
-			System.out.println("woman");
-		else
-			System.out.println("baby");
+//		Person man = Person.WOMAN;
+//		if (man == Person.MAN)
+//			System.out.println("man");
+//		else if (man == Person.WOMAN)
+//			System.out.println("woman");
+//		else
+//			System.out.println("baby");
 
 //		if (man == Animal.CAT) {
 //			System.out.println("야옹");
