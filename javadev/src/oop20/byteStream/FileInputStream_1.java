5af1487e0fa87b3ad785128e4bab50e7a14eb1ac
cookooyas <cookooyas@gmail.com>
2024-06-26 16:51:21 +0900

    byte based input stream

---
+++ javadev/src/oop20/byteStream/FileInputStream_1.java
@@ -0,0 +1,18 @@
+package oop20.byteStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileInputStream_1 {
+	public static void main(String[] args) throws IOException {
+		File inFile = new File("src/oop20/byteStream/FileInputStream1.txt");
+		InputStream is = new FileInputStream(inFile);
+		int data;
+		while ((data = is.read()) != -1) {
+			System.out.println("읽은 데이터 : " + (char) data + ", 남은 바이트 수 : " + is.available());
+		}
+		is.close();
+	}
+}
