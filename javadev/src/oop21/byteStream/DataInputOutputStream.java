66a364860cf4cf47f6e8b7e966070f4377e9e287
cookooyas <cookooyas@gmail.com>
2024-06-27 14:24:53 +0900

    data(or combined) I/O

---
+++ javadev/src/oop21/byteStream/DataInputOutputStream.java
@@ -0,0 +1,43 @@
+package oop21.byteStream;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DataInputOutputStream {
+	public static void main(String[] args) {
+		File data = new File("src/oop21/byteStream/data.data");
+		try (OutputStream os = new FileOutputStream(data); DataOutputStream dos = new DataOutputStream(os);) {
+			dos.writeInt(22);
+			dos.writeDouble(3.3324);
+			dos.writeChar('a');
+			dos.writeChar('1');
+			dos.writeChar('2');
+			dos.writeChar('3');
+			dos.writeChar('4');
+			dos.writeUTF("이게 뭐여");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try (InputStream is = new FileInputStream(data); DataInputStream dis = new DataInputStream(is);) {
+			System.out.println(dis.readInt());
+			System.out.println(dis.readDouble());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readUTF());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
